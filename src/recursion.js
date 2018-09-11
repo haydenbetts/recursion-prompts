520a39074d8fbcc7a8f21dd8ab2a88dc7ab9e51f
haydenbetts <haydenbetts@gmail.com>
2018-09-11 12:59:53 -0700

    range

--- src/recursion.js
+++ src/recursion.js
@@ -64,7 +64,31 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+
+    if (x === y) {
+        return [];
+    }
+
+    // handle x < y
+    if (x < y) {
+        if (x + 1 === y) {
+            return [];
+        }
+
+        return [x + 1].concat(range(x + 1, y));
+    }
+
+    // handle y < x
+
+    if (y < x) {
+        if (x - 1 === y) {
+            return [];
+        }
+
+        return [x - 1].concat(range(x - 1, y));
+    }
 };
+
 
 // 7. Compute the exponent of a number.
 // The exponent of a number says how many times the base number is used as a factor.
