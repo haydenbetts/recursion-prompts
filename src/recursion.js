3b7e0013dff5abe51507d8c828f98b8ef0c54501
haydenbetts <haydenbetts@gmail.com>
2018-09-11 14:02:01 -0700

    palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -146,6 +146,17 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+    string = string.replace(/\s/g, '');
+
+    if (string.length === 1 || string.length === 0) {
+        return true;
+    }
+
+    if (string[0].toLowerCase() !== string.slice(string.length - 1).toLowerCase()) {
+        return false;
+    }
+
+    return true && palindrome(string.slice(1, string.length - 1));
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
